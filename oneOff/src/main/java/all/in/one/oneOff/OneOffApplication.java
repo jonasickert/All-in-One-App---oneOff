87036579b777a80c0943e40b93bbf5ee831c2845
EchoNexis <jonas0ickert@gmail.com>
2024-11-25 10:26:16 +0100

    initial commit

---
+++ oneOff/src/main/java/all/in/one/oneOff/OneOffApplication.java
@@ -0,0 +1,13 @@
+package all.in.one.oneOff;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OneOffApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OneOffApplication.class, args);
+	}
+
+}
---
+++ oneOff/src/test/java/all/in/one/oneOff/OneOffApplicationTests.java
@@ -0,0 +1,13 @@
+package all.in.one.oneOff;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OneOffApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
